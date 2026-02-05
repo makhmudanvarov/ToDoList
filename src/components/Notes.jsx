8199bd7ed75a943a93bca64a665db5bdc6d8d911
Makhmud Anvarov <makhmudanvarov20050108@gmail.com>
2026-02-05 22:31:45 +0500

    done

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,73 +1,79 @@
-import { useState } from 'react'
-import gridIcon from '../assets/images/grid.svg'
-import listIcon from '../assets/images/list.svg'
-import pencil from '../assets/images/pencil.svg'
-import trash from '../assets/images/trash.svg'
-import { useTodoStore } from '../store'
+import { useState } from "react";
+import gridIcon from "../assets/images/grid.svg";
+import listIcon from "../assets/images/list.svg";
+import pencil from "../assets/images/pencil.svg";
+import trash from "../assets/images/trash.svg";
+import { useTodoStore } from "../store";
 
 const Notes = () => {
-    const [grid, setGrid] = useState(true)
-    const removeNote = useTodoStore(state => state.removeNote)
-    const setUpdate = useTodoStore(state => state.setUpdate)
-    const setModal = useTodoStore(state => state.setModal)
-    const setTitle = useTodoStore(state => state.setTitle)
-    const setText = useTodoStore(state => state.setText)
-    const notes = useTodoStore(state => state.notes)
-    const lang = useTodoStore(state => state.lang)
+  const [grid, setGrid] = useState(true);
+  const removeNote = useTodoStore((state) => state.removeNote);
+  const setUpdate = useTodoStore((state) => state.setUpdate);
+  const setModal = useTodoStore((state) => state.setModal);
+  const setTitle = useTodoStore((state) => state.setTitle);
+  const setText = useTodoStore((state) => state.setText);
+  const notes = useTodoStore((state) => state.notes);
+  const lang = useTodoStore((state) => state.lang);
 
-    const updateNote = (note) => {
-        setModal(true)
-        setUpdate({ edit: true, note: note })
-        setTitle(note.title)
-        setText(note.text)
-    }
-    if(notes.length) {
-        return (
-            <div className="notes">
-                <div className="container">
-                    <div className="notes_top">
-                        <h2 className="notes_top_title">{lang.allNotes}</h2>
-                        <button className="notes_top_btn" onClick={() => setGrid(!grid)}>
-                            {
-                                grid ?
-                                    <>
-                                        <img src={listIcon} alt="" />
-                                        <span>{lang.list}</span>
-                                    </>
-                                    : <>
-                                        <img src={gridIcon} alt="" />
-                                        <span>{lang.grid}</span>
-                                    </>
-                            }
-                        </button>
-                    </div>
-                    <div className={`notes_box ${!grid && 'active'}`}>
-                        {notes.map(note => (
-                            <div className="notes_card" key={note.id}>
-                                <div className="notes_card_info">
-                                    <h3 className="notes_card_title">{note.title}</h3>
-                                    <p className="notes_card_date">{note.date}</p>
-                                </div>
-                                <p className="notes_card_text">{note.text}</p>
-                                <div className="notes_card_btns">
-                                    <button className="btn btn_primary" onClick={() => updateNote(note)}>
-                                        <img src={pencil} alt="" />
-                                        {lang.edit}
-                                    </button>
-                                    <button className="btn btn_danger" onClick={() => removeNote(note.id)}>
-                                        <img src={trash} alt="" />
-                                        {lang.delete}
-                                    </button>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+  const updateNote = (note) => {
+    setModal(true);
+    setUpdate({ edit: true, note: note });
+    setTitle(note.title);
+    setText(note.text);
+  };
+  if (notes.length) {
+    return (
+      <div className="notes">
+        <div className="container">
+          <div className="notes_top">
+            <h2 className="notes_top_title">{lang.allNotes}</h2>
+            <button className="notes_top_btn" onClick={() => setGrid(!grid)}>
+              {grid ? (
+                <>
+                  <img src={listIcon} alt="" />
+                  <span>{lang.list}</span>
+                </>
+              ) : (
+                <>
+                  <img src={gridIcon} alt="" />
+                  <span>{lang.grid}</span>
+                </>
+              )}
+            </button>
+          </div>
+          <div className={`notes_box ${!grid && "active"}`}>
+            {notes.map((note) => (
+              <div className="notes_card" key={note.id}>
+                <div className="notes_card_info">
+                  <h3 className="notes_card_title">{note.title}</h3>
+                  <p className="notes_card_date">{note.date}</p>
                 </div>
-            </div>
-        )
-    } else {
-        return <h1 className='noNote'>{lang.noNotes}</h1>
-    }
-}
+                <p className="notes_card_text">{note.text}</p>
+                <div className="notes_card_btns">
+                  <button
+                    className="btn btn_primary"
+                    onClick={() => updateNote(note)}
+                  >
+                    <img src={pencil} alt="" />
+                    {lang.edit}
+                  </button>
+                  <button
+                    className="btn btn_danger"
+                    onClick={() => removeNote(note.id)}
+                  >
+                    <img src={trash} alt="" />
+                    {lang.delete}
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return <h1 className="noNote">{lang.noNotes}</h1>;
+  }
+};
 
-export default Notes
+export default Notes;
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,19 @@
-import { create } from "zustand";
+import { create } from "zustand"
 import { uz } from '../utils/lang.js'
+
+const getNotes = () => {
+    let local = localStorage.getItem('notes')
+    if (local) {
+        return JSON.parse(local)
+    } else {
+        return []
+    }
+}
 
 export const useTodoStore = create((set) => ({
     lang: uz,
     update: { edit: false, note: null },
-    notes: [
-        { id: 1, title: "Salom", text: "Nma gap1", date: new Date().toLocaleDateString(), },
-        { id: 2, title: "Hayr", text: "Nma gap2", date: new Date().toLocaleDateString(), },
-        { id: 3, title: "Tugadi", text: "Nma gap3", date: new Date().toLocaleDateString(), },
-    ],
+    notes: getNotes(),
     removeNote: (id) =>
         set((state) => ({
             notes: state.notes.filter((note) => note.id !== id),
@@ -18,7 +23,10 @@ export const useTodoStore = create((set) => ({
     modal: false,
     setUpdate: (data) => set({ update: data }),
     setLang: (data) => set({ lang: data }),
-    setNotes: (data) => set({ notes: data }),
+    setNotes: (data) => set(() => {
+        localStorage.setItem('notes', JSON.stringify(data))
+        return { notes: data }
+    }),
     setModal: (data) => set({ modal: data }),
     setTitle: (data) => set({ title: data }),
     setText: (data) => set({ text: data }),
